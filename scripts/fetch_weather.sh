fa657260def2325463b41096dca6e6adde0014ef
Chuck Hennet <dba.hennet@gmail.com>
2025-07-26 07:53:41 -0400

    Update fetch_weather.sh to use clean Python install

--- scripts/fetch_weather.sh
+++ scripts/fetch_weather.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-
-
 # Log start
 echo "[`date`] 🌤️ Starting fetch_weather.sh" >> /Users/chuck/BirdMonitor/logs/weather.log
-# Suppress NotOpenSSLWarning globally
-PYTHONWARNINGS="ignore::NotOpenSSLWarning"
 
-# Run the weather data fetch script using system Python
-/usr/local/bin/python3 /Users/chuck/BirdMonitor/scripts/fetch_weather_data.py >> /Users/chuck/BirdMonitor/logs/weather.log 2>&1
-
+/opt/homebrew/bin/python3 /Users/chuck/BirdMonitor/scripts/fetch_weather_data.py >> /Users/chuck/BirdMonitor/logs/weather.log 2>&1
 
